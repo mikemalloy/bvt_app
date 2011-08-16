8e99333cd2ba12c864d39fc49c9402108c7ad843
mikemalloy <mmalloy@zynga.com>
2011-08-16 12:26:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BvtApp::Application.config.secret_token = '78581c28d858697a5ece1a862a6f6122000fb5483dc5efe08c83c9bd73e8f28c5a15ced46413b123e4cdfcb4c48b99c11aa4fb2dd49a104c5fd3b8f73a4f1b50'
