8228c7751f876f37ac18dfe57a932b87adc19980
mikemalloy <mmalloy@zynga.com>
2011-08-16 14:31:15 -0700

    Added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 BvtApp::Application.routes.draw do
-  match '/contact' :to 'pages/contact'
+  get "users/new"
+
+  match '/contact', :to =>'pages#contact'
   
   get "pages/home"
 
@@ -54,7 +56,7 @@ BvtApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages/home'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
